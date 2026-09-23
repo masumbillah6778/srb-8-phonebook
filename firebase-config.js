7083a390caf0c1059a6fd42821b2691f2b468de8
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-09-23 16:20:49 +0600

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyC-hTxL3EwdAsvs0SmW1dF7y62NiCao7x0",
-  authDomain: "srb-8-c9411.firebaseapp.com",
-  projectId: "srb-8-c9411",
-  storageBucket: "srb-8-c9411.firebasestorage.app",
-  messagingSenderId: "326071090889",
-  appId: "1:326071090889:web:b8a61545e1afb87c925718"
-};
-firebase.initializeApp(firebaseConfig);
